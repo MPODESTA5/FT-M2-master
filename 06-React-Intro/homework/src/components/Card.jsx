1f603192694a15ea42048630cedadd7aad5353ce
MPODESTA5 <mrtnpodesta@gmail.com>
2022-10-01 23:39:46 -0300

    Tarea realizada

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,20 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  //return <div>Card Component</div>
+  
+  return (
+    <div>
+      <button onClick={props.onClose}>X</button>
+      <h2>{props.name}</h2>
+      <div>
+         <h4>Min</h4>
+         <p>{props.min}</p>
+         <h4>Max</h4>
+         <p>{props.max}</p>
+      </div>
+         <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt='Ilustración del clima'/>
+    </div>
+  )
+          
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,6 +1,13 @@
+//import { Button } from 'bootstrap';
 import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  //return <div>Search Bar Component</div>
+return (
+    <div>
+      <input type='text' placeholder='Ciudad...'></input>
+      <button onClick={()=>props.onSearch('Buscando Ciudad')}>Agregar</button>
+    </div>
+)
 };
